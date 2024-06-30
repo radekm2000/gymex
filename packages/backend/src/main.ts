ffa6a5bedb2452c516750b2185dea4706b0761f7
Radek <104318242+radekm2000@users.noreply.github.com>
2024-06-30 18:29:00 +0200

    add cors

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
+
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
