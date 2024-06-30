b2efbeec2041c3e5a8107cf33d951a4104c61d36
Radek <104318242+radekm2000@users.noreply.github.com>
2024-06-30 20:05:37 +0200

    add shadcn

--- packages/webapp/src/App.tsx
+++ packages/webapp/src/App.tsx
@@ -1,10 +1,7 @@
 function App() {
   return (
     <>
-      <div className="bg-white hover:bg-cyan-600">
-        <span className="font-display font-light text-cyan-600">hello world</span>
-      </div>
-
+      <p>hello world</p>
     </>
   );
 }
--- packages/webapp/tailwind.config.js
+++ packages/webapp/tailwind.config.js
@@ -1,22 +1,51 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        primary: {
-          light: "#262626",
-          DEFAULT: "#202020",
-          dark: "#151515",
-        },
-      },
-      fontFamily: {
-        display: "'Readex Pro', sans-serif",
-      },
-      fontWeight: {
-        light: "300",
-      },
+export const darkMode = ["class"];
+export const content = [
+  "./pages/**/*.{ts,tsx}",
+  "./components/**/*.{ts,tsx}",
+  "./app/**/*.{ts,tsx}",
+  "./src/**/*.{ts,tsx}",
+  "./index.html",
+];
+export const prefix = "";
+export const theme = {
+  container: {
+    center: true,
+    padding: "2rem",
+    screens: {
+      "2xl": "1400px",
     },
   },
-  plugins: [],
+  extend: {
+    keyframes: {
+      "accordion-down": {
+        from: { height: "0" },
+        to: { height: "var(--radix-accordion-content-height)" },
+      },
+      "accordion-up": {
+        from: { height: "var(--radix-accordion-content-height)" },
+        to: { height: "0" },
+      },
+    },
+    animation: {
+      "accordion-down": "accordion-down 0.2s ease-out",
+      "accordion-up": "accordion-up 0.2s ease-out",
+    },
+
+    colors: {
+      primary: {
+        light: "#262626",
+        DEFAULT: "#202020",
+        dark: "#151515",
+      },
+    },
+    fontFamily: {
+      display: "'Readex Pro', sans-serif",
+    },
+    fontWeight: {
+      light: "300",
+    },
+  },
 };
+// eslint-disable-next-line no-undef
+export const plugins = [require("tailwindcss-animate")];
