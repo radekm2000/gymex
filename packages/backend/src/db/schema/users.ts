9a100e993511d695e0a5db3dd0cce40a6d9a9111
Radek <104318242+radekm2000@users.noreply.github.com>
2024-07-02 17:39:41 +0200

    add UsersMetrics table

--- packages/backend/src/db/schema/users.ts
+++ packages/backend/src/db/schema/users.ts
@@ -1,7 +1,22 @@
-import { pgTable, serial, text, uuid } from 'drizzle-orm/pg-core';
+import { pgTable, serial, text, timestamp, uuid } from 'drizzle-orm/pg-core';
 
 export const UsersTable = pgTable('users', {
   id: serial('id').primaryKey(),
   username: text('username').notNull(),
-  age: text('age').notNull(),
+  createdAt: timestamp('created_at', {
+    mode: 'date',
+  }),
+  discordId: text('discord_id'),
+  googleId: text('google_id'),
+});
+
+export const UsersMetricsTable = pgTable('user_metrics', {
+  userId: text('user_id')
+    .primaryKey()
+    .references(() => UsersTable.id, {
+      onDelete: 'cascade',
+    }),
+
+  weight: text('weight'),
+  height: text('height'),
 });
