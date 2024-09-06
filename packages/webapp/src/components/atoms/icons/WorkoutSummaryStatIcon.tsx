c95ef1a5f3c3170fe48f807bc4477e4d9381c385
Radek <104318242+radekm2000@users.noreply.github.com>
2024-09-06 20:32:37 +0200

    add custom summary stat icon

---
+++ packages/webapp/src/components/atoms/icons/WorkoutSummaryStatIcon.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from "react";
+
+type Props = {
+  icon: ReactNode;
+  title: string;
+  data: string | number;
+  suffix?: string;
+};
+
+export const WorkoutSummaryStatIcon = ({ icon, title, data, suffix }: Props) => {
+  return (
+    <div className="flex gap-2 text-opacity-70 text-border-default ">
+      <div className="flex items-center justify-center">{icon}</div>
+      <div className="flex flex-col items-start justify-center">
+        <span className="text-sm font-display">{title}</span>
+        <span className="text-lg text-neutral-950 font-display">{data} {suffix}</span>
+      </div>
+    </div>
+  );
+};
--- packages/webapp/src/components/pages/WorkoutsHistory.tsx
+++ packages/webapp/src/components/pages/WorkoutsHistory.tsx
@@ -1,3 +1,15 @@
+import { Timer } from "lucide-react";
+import { WorkoutSummaryStatIcon } from "../atoms/icons/WorkoutSummaryStatIcon";
+
 export const WorkoutsHistory = () => {
-  return <div>WorkoutsHistory</div>;
+  return (
+    <div className="flex flex-col items-center justify-center bg-textInput-light h-[400px] bg-opacity-90">
+      <span className="text-2xl text-white">siertemreoraeorao</span>
+      <WorkoutSummaryStatIcon
+        icon={<Timer size={42} />}
+        data={"01:13:16"}
+        title="TIME DURATION"
+      />
+    </div>
+  );
 };
