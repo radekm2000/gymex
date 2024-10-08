0f42f50cc9eeebf1299959313f595d3a4ac4de01
Radek <104318242+radekm2000@users.noreply.github.com>
2024-10-08 09:26:28 +0200

    change fading tooltip brightness

--- packages/webapp/src/components/atoms/tooltip/FadingTooltip.tsx
+++ packages/webapp/src/components/atoms/tooltip/FadingTooltip.tsx
@@ -21,16 +21,16 @@ export const FadingTooltip = ({
 }: Props) => {
   return (
     <TooltipProvider disableHoverableContent={true}>
-      <Tooltip delayDuration={0}>
+      <Tooltip >
         <TooltipTrigger asChild>{children}</TooltipTrigger>
         <TooltipPortal>
           <TooltipContent
-            className="z-50 data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade text-white select-none rounded-[4px] bg-secondary-customGray px-[15px] py-[8px] text-[15px] leading-none shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity] font-display font-thin"
+            className="z-50 data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade text-white select-none  rounded-[4px] bg-secondary-customGray opacity-80 px-[15px] py-[8px] text-[15px] leading-none shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity] font-display font-thin"
             side={tooltipPlacement}
             sideOffset={5}
           >
             <p>{tooltipText}</p>
-            <TooltipArrow className="fill-secondary-customGray" />
+            <TooltipArrow className="fill-secondary-customGray opacity-80" />
           </TooltipContent>
         </TooltipPortal>
       </Tooltip>
