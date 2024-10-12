f384043d8d792eba20c45f191376bde538d9fc04
Radek <104318242+radekm2000@users.noreply.github.com>
2024-10-12 12:11:24 +0200

    sort workouts by finished at date

--- packages/webapp/src/components/molecules/workout-history/WorkoutHistoryMonthCardExpandedContent.tsx
+++ packages/webapp/src/components/molecules/workout-history/WorkoutHistoryMonthCardExpandedContent.tsx
@@ -8,9 +8,16 @@ type Props = {
 };
 
 export const WorkoutHistoryMonthCardExpandedContent = ({ workouts }: Props) => {
+  const sortedWorkouts = workouts.sort((a, b) => {
+    const timeA = new Date(a.detailedWorkoutModel.session.finishedAt).getTime();
+    const timeB = new Date(b.detailedWorkoutModel.session.finishedAt).getTime();
+
+    return timeA - timeB;
+  });
+
   return (
     <Card className="flex flex-col gap-2 p-0 bg-opacity-100 border-t-0 rounded-t-none bg-neutral-100 ">
-      {workouts.map((workout, index) => (
+      {sortedWorkouts.map((workout, index) => (
         <div key={index}>
           <WorkoutSessionItem workout={workout} />
           {index !== workouts.length - 1 && <Separator />}
