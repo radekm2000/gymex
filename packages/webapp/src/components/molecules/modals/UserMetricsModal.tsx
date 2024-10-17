c70508d1c150cf76a1d5a2f2f576fc43bf685e50
Radek <104318242+radekm2000@users.noreply.github.com>
2024-10-17 14:50:49 +0200

    user metrics dialog fix

--- packages/webapp/src/components/molecules/modals/UserMetricsModal.tsx
+++ packages/webapp/src/components/molecules/modals/UserMetricsModal.tsx
@@ -15,7 +15,7 @@ import { LabeledInput } from "../../atoms/inputs/LabeledInput";
 import { useUserMetricsUpdateMutation } from "../../../api/mutations/user";
 
 export const UserMetricsModal = () => {
-  const { user, isAuthed } = useAuth();
+  const { user } = useAuth();
   const userId = user.model.user.id;
   const isUserFirstTimeLoggedIn = user.isUserFirstTimeLoggedIn;
   const updateUserMetricsMutation = useUserMetricsUpdateMutation();
@@ -28,50 +28,48 @@ export const UserMetricsModal = () => {
     updateUserMetricsMutation.mutate({ dto, userId });
   };
 
-  if (!isAuthed) {
-    return <NotAuthed />;
-  }
-
   return (
-    <AlertDialog
-      open={isUserFirstTimeLoggedIn}
-      onOpenChange={(open) => open === isUserFirstTimeLoggedIn}
-    >
-      <AlertDialogContent>
-        <AlertDialogHeader>
-          <AlertDialogTitle>
-            Fill in your user metrics and set a display name if you want to show
-            it instead of your discord username
-          </AlertDialogTitle>
-          <AlertDialogDescription>
-            <LabeledInput
-              label="Display name"
-              inputValue={displayName}
-              onInputValueChange={setDisplayName}
-            />
-            <LabeledInput
-              label="Weight"
-              inputValue={weight}
-              onInputValueChange={setWeight}
-            />
-            <LabeledInput
-              label="Height"
-              inputValue={height}
-              onInputValueChange={setHeight}
-            />
-            <span className="text-sm text-slate-400">
-              All inputs are optional and you can leave them blank.
-            </span>
-          </AlertDialogDescription>
-        </AlertDialogHeader>
-        <AlertDialogFooter>
-          <AlertDialogAction
-            onClick={() => onUpdate({ displayName, height, weight }, userId)}
-          >
-            Continue
-          </AlertDialogAction>
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
+    user && (
+      <AlertDialog
+        open={isUserFirstTimeLoggedIn}
+        onOpenChange={(open) => open === isUserFirstTimeLoggedIn}
+      >
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>
+              Fill in your user metrics and set a display name if you want to
+              show it instead of your discord username
+            </AlertDialogTitle>
+            <AlertDialogDescription>
+              <LabeledInput
+                label="Display name"
+                inputValue={displayName}
+                onInputValueChange={setDisplayName}
+              />
+              <LabeledInput
+                label="Weight"
+                inputValue={weight}
+                onInputValueChange={setWeight}
+              />
+              <LabeledInput
+                label="Height"
+                inputValue={height}
+                onInputValueChange={setHeight}
+              />
+              <span className="text-sm text-slate-400">
+                All inputs are optional and you can leave them blank.
+              </span>
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogAction
+              onClick={() => onUpdate({ displayName, height, weight }, userId)}
+            >
+              Continue
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    )
   );
 };
